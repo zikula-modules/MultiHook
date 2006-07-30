4b4519eb9ff144ac90e69780a1c0afafe33cab24
landseer <frank.schummertz@gmail.com>
2006-07-30 10:49:39 +0000

    added missing version.php files

---
+++ MultiHook/pnlang/eng/version.php
@@ -0,0 +1,22 @@
+<?php
+// $Id: init.php 37 2006-01-16 12:48:39Z landseer $
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+
+define('_MH_MODULENAME','MultiHook');
+define('_MH_MODULEDESCRIPTION','Creates xhtml tags for abbreviations, acronyms and autolinks');
+
+?>
