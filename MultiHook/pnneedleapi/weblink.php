1f6e67045b7eaf787289e2f9e762b9cca4352f27
landseer <frank.schummertz@gmail.com>
2006-09-16 20:26:32 +0000

    added needle for Web_Links

--- MultiHook/pnneedleapi/weblink.php
+++ MultiHook/pnneedleapi/weblink.php
@@ -20,11 +20,11 @@
 // ----------------------------------------------------------------------
 
 /**
- * download needle
+ * weblink needle
  * @param $args['nid'] needle id
  * @return array()
  */
-function MultiHook_needleapi_download($args)
+function MultiHook_needleapi_weblink($args)
 {
     // Get arguments from argument array
     $nid = $args['nid'];
@@ -36,12 +36,12 @@ function MultiHook_needleapi_download($args)
         $cache = array();
     } 
 
-    $result = '<em title="' . pnVarPrepForDisplay(sprintf(_MH_NEEDLEDATAERROR, $nid, 'Downloads')) . '">DOWNLOAD' . $nid . '</em>';
+    $result = '<em title="' . pnVarPrepForDisplay(sprintf(_MH_NEEDLEDATAERROR, $nid, 'Web_Links')) . '">WEBLINK' . $nid . '</em>';
     if(!isset($cache[$nid])) {
         // not in cache array
         // set the default
         $cache[$nid] = $result;
-        if(pnModAvailable('Downloads')) {
+        if(pnModAvailable('Web_Links')) {
             // nid is like C_##, D_## or L_##
             $temp = explode('_', $nid);
             $type = '';
@@ -50,22 +50,22 @@ function MultiHook_needleapi_download($args)
                 $id   = $temp[1];
             }
         
-            pnModDBInfoLoad('Downloads');
+            pnModDBInfoLoad('Web_Links');
             $dbconn =& pnDBGetConn(true);
             $pntable =& pnDBGetTables();
             
             switch($type) {
                 case 'C':
-                    $tbldlcats = $pntable['downloads_categories'];
-                    $coldlcats = $pntable['downloads_categories_column'];
+                    $tblwlcats = $pntable['links_categories'];
+                    $colwlcats = $pntable['links_categories_column'];
                     
-                    $sql = 'SELECT ' . $coldlcats['title'] . ', ' . $coldlcats['cdescription'] . ' FROM ' . $tbldlcats . ' WHERE ' . $coldlcats['cid'] . '=' . pnVarPrepForStore($id);
+                    $sql = 'SELECT ' . $colwlcats['title'] . ', ' . $colwlcats['cdescription'] . ' FROM ' . $tblwlcats . ' WHERE ' . $colwlcats['cat_id'] . '=' . pnVarPrepForStore($id);
                     $res = $dbconn->Execute($sql);
                     if($dbconn->ErrorNo()==0 && !$res->EOF) {
                         list($title, $desc) = $res->fields;
                         list($url,
                              $title,
-                             $desc) = pnVarPrepForDisplay('index.php?name=Downloads&req=viewdownload&cid=' . $id,
+                             $desc) = pnVarPrepForDisplay('index.php?name=Web_Links&req=viewlink&cid=' . $id,
                                                           $title,
                                                           $desc);
                         $cache[$nid] = '<a href="' . $url . '" title="' . $desc . '">' . $title . '</a>';
@@ -73,17 +73,17 @@ function MultiHook_needleapi_download($args)
                     break;
                 case 'D':
                 case 'L':
-                    $tbldls = $pntable['downloads_downloads'];
-                    $coldls = $pntable['downloads_downloads_column'];
+                    $tblwls = $pntable['links_links'];
+                    $colwls = $pntable['links_links_column'];
                     
-                    $sql = 'SELECT ' . $coldls['title'] . ', ' . $coldls['description'] . ' FROM ' . $tbldls . ' WHERE ' . $coldls['lid'] . '=' . pnVarPrepForStore($id);
+                    $sql = 'SELECT ' . $colwls['title'] . ', ' . $colwls['description'] . ' FROM ' . $tblwls . ' WHERE ' . $colwls['lid'] . '=' . pnVarPrepForStore($id);
                     $res = $dbconn->Execute($sql);
                     if($dbconn->ErrorNo()==0 && !$res->EOF) {
                         list($title, $desc) = $res->fields;
                         if($type=='D') {
-                            $url = 'index.php?name=Downloads&req=viewdownload&cid=' . $id;
+                            $url = 'index.php?name=Web_Links&req=viewlinkdetails&lid=' . $id;
                         } else {
-                            $url = 'index.php?name=Downloads&req=getit&lid=' . $id;
+                            $url = 'index.php?name=Web_Links&req=visit&lid=' . $id;
                         }
                         list($url,
                              $title,
