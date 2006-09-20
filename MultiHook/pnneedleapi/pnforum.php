007ffb09048c0314639e436ba82224ba69914f33
landseer <frank.schummertz@gmail.com>
2006-09-20 21:14:28 +0000

    validity checks, better error messages

--- MultiHook/pnneedleapi/pnforum.php
+++ MultiHook/pnneedleapi/pnforum.php
@@ -36,57 +36,85 @@ function MultiHook_needleapi_pnforum($args)
         $cache = array();
     } 
 
-    $result = '<em title="' . pnVarPrepForDisplay(sprintf(_MH_NEEDLEDATAERROR, $nid, 'pnForum')) . '">PNFORUM' . $nid . '</em>';
-    if(!isset($cache[$nid])) {
-        // not in cache array
-        // set the default
-        $cache[$nid] = $result;
-        if(pnModAvailable('pnForum')) {
-            
-            // nid is like C_## or T_##
-            $temp = explode('_', $nid);
-            $type = '';
-            if(is_array($temp) && count($temp)==2) {
-                $type = $temp[0];
-                $id   = $temp[1];
-            }
-            
-            pnModDBInfoLoad('pnForum');
-            $dbconn =& pnDBGetConn(true);
-            $pntable =& pnDBGetTables();
-
-            switch($type) {
-                case 'F':
-                    $tblforums = $pntable['pnforum_forums'];
-                    $colforums = $pntable['pnforum_forums_column'];
-                    
-                    $sql = 'SELECT ' . $colforums['forum_name'] . ' FROM ' . $tblforums . ' WHERE ' . $colforums['forum_id'] . '=' . pnVarPrepForStore($id);
-                    $res = $dbconn->Execute($sql);
-                    if($dbconn->ErrorNo()==0 && !$res->EOF) {
-                        list($title) = $res->fields;
-                        $url   = pnVarPrepForDisplay(pnModURL('pnForum', 'user', 'viewforum', array('forum' => $id)));
-                        $title = pnVarPrepForDisplay($title);
-                        $cache[$nid] = '<a href="' . $url . '" title="' . $title . '">' . $title . '</a>';
-                    }
-                    break;
-                case 'T':
-                    $tbltopics = $pntable['pnforum_topics'];
-                    $coltopics = $pntable['pnforum_topics_column'];
-                    
-                    $sql = 'SELECT ' . $coltopics['topic_title'] . ' FROM ' . $tbltopics . ' WHERE ' . $coltopics['topic_id'] . '=' . pnVarPrepForStore($id);
-                    $res = $dbconn->Execute($sql);
-                    if($dbconn->ErrorNo()==0 && !$result->EOF) {
-                        list($title) = $res->fields;
-                        $url   = pnVarPrepForDisplay(pnModURL('pnForum', 'user', 'viewtopic', array('topic' => $id)));
-                        $title = pnVarPrepForDisplay($title);
-                        $cache[$nid] = '<a href="' . $url . '" title="' . $title . '">' . $title . '</a>';
-                    }
-                    break;
-                default:
-                    // $cache[$nid] = $result; is already done
+    pnModLangLoad('MultiHook', 'pnforum');
+    if(!empty($nid)) {
+        if(!isset($cache[$nid])) {
+            // not in cache array
+            // set the default
+            $cache[$nid] = $result;
+            if(pnModAvailable('pnForum')) {
+                
+                // nid is like F_## or T_##
+                $temp = explode('_', $nid);
+                $type = '';
+                if(is_array($temp) && count($temp)==2) {
+                    $type = $temp[0];
+                    $id   = $temp[1];
+                }
+                
+                include_once 'modules/pnForum/common.php';
+                pnModDBInfoLoad('pnForum');
+                $dbconn =& pnDBGetConn(true);
+                $pntable =& pnDBGetTables();
+        
+                switch($type) {
+                    case 'F':
+                        $tblforums = $pntable['pnforum_forums'];
+                        $colforums = $pntable['pnforum_forums_column'];
+                        
+                        $sql = 'SELECT ' . $colforums['forum_name'] . ',
+                                       ' . $colforums['cat_id'] . '
+                                FROM   ' . $tblforums . '
+                                WHERE  ' . $colforums['forum_id'] . '=' . (int)pnVarPrepForStore($id);
+                        $res = $dbconn->Execute($sql);
+                        if($dbconn->ErrorNo()==0 && !$res->EOF) {
+                            list($title, $cat_id) = $res->fields;
+                            if(allowedtoreadcategoryandforum($cat_id, $id)) {
+                                $url   = pnVarPrepForDisplay(pnModURL('pnForum', 'user', 'viewforum', array('forum' => $id)));
+                                $title = pnVarPrepForDisplay($title);
+                                $cache[$nid] = '<a href="' . $url . '" title="' . $title . '">' . $title . '</a>';
+                            } else {
+                                $cache[$nid] = '<em>' . pnVarPrepForDisplay(MH_PNF_NOAUTHFORFORUM . ' (' . $id . ')') . '</em>';
+                            }
+                        } else {
+                            $cache[$nid] = '<em>' . pnVarPrepForDisplay(MH_PNF_UNKNOWNFORUM . ' (' . $id . ')') . '</em>';
+                        }
+                        break;
+                    case 'T':
+                        $tbltopics = $pntable['pnforum_topics'];
+                        $coltopics = $pntable['pnforum_topics_column'];
+                        $tblforums = $pntable['pnforum_forums'];
+                        $colforums = $pntable['pnforum_forums_column'];
+                        
+                        $sql = 'SELECT    ' . $coltopics['topic_title'] . ',
+                                          ' . $coltopics['forum_id'] . ',
+                                          ' . $colforums['cat_id'] . ' 
+                                FROM      ' . $tbltopics . '
+                                LEFT JOIN ' . $tblforums . '
+                                ON        ' . $colforums['forum_id'] . '=' . $coltopics['forum_id'] . '
+                                WHERE     ' . $coltopics['topic_id'] . '=' . pnVarPrepForStore($id);
+                        $res = $dbconn->Execute($sql);
+                        if($dbconn->ErrorNo()==0 && !$result->EOF) {
+                            list($title, $forum_id, $cat_id) = $res->fields;
+                            if(allowedtoreadcategoryandforum($cat_id, $forum_id)) {
+                                $url   = pnVarPrepForDisplay(pnModURL('pnForum', 'user', 'viewtopic', array('topic' => $id)));
+                                $title = pnVarPrepForDisplay($title);
+                                $cache[$nid] = '<a href="' . $url . '" title="' . $title . '">' . $title . '</a>';
+                            } else {
+                                $cache[$nid] = '<em>' . pnVarPrepForDisplay(MH_PNF_NOAUTHFORTOPIC . ' (' . $id . ')') . '</em>';
+                            }
+                        } else {
+                            $cache[$nid] = '<em>' . pnVarPrepForDisplay(MH_PNF_UNKNOWNTOPIC . ' (' . $id . ')') . '</em>';
+                        }
+                        break;
+                    default:
+                        $cache[$nid] = '<em>' . pnVarPrepForDisplay(MH_PNF_UNKNOWNTYPE) . '</em>';
+                }
             }
         }
         $result = $cache[$nid];
+    } else {
+        $result = '<em>' . pnVarPrepForDisplay(_MH_PNF_NONEEDLEID) . '</em>';
     }
     return $result;
     
