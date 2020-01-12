f7cadc9934ce4338e5d6ef972b7d56458a9f98ae
Guite <Guite@users.noreply.github.com>
2020-01-12 09:49:20 +0000

    update generated code :rocket:

--- modules/Zikula/MultiHookModule/Form/Handler/Entry/Base/AbstractEditHandler.php
+++ modules/Zikula/MultiHookModule/Form/Handler/Entry/Base/AbstractEditHandler.php
@@ -64,10 +64,8 @@ abstract class AbstractEditHandler extends EditHandler
             return new RedirectResponse($this->getRedirectUrl(['commandName' => '']), 302);
         }
     
-        $entityData = $this->entityRef->toArray();
-    
-        // assign data to template as array (for additions like standard fields)
-        $this->templateParameters[$this->objectTypeLower] = $entityData;
+        // assign data to template (for additions like standard fields)
+        $this->templateParameters[$this->objectTypeLower] = $this->entityRef;
         $this->templateParameters['supportsHookSubscribers'] = $this->entityRef->supportsHookSubscribers();
     
         return $result;
